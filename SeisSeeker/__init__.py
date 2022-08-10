33ff804eae4e40adce2a331a3c0a02d900194077
“TomSHudson” <tomhudson92@gmail.com>
2022-08-10 12:07:43 +0100

    Initial package commit.

---
+++ SeisSeeker/__init__.py
@@ -0,0 +1,11 @@
+name = "SeisSeeker"
+__version__ = "0.0.1"
+__description__ = "SeisSeeker - A package for performing earthquake detection using arrays."
+__license__ = "MIT"
+__author__ = "Tom Hudson"
+__email__ = "thomas.hudson@earth.ox.ac.uk"
+
+# Import neccessary modules:
+from SeisSeeker import io
+from SeisSeeker import processing
+from SeisSeeker import plot 
---
+++ SeisSeeker/processing/detection.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+# Script Description:
+# Script description here...
+
+# Input variables:
+
+# Output variables:
+
+# Created by Tom Hudson, insert_date_here
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+# Import neccessary modules:
+
+
+#----------------------------------------------- Define constants and parameters -----------------------------------------------
+# Define any constants/parameters:
+# Define any constants and parameters used here...
+
+#----------------------------------------------- End: Define constants and parameters -----------------------------------------------
+
+
+#----------------------------------------------- Define main functions -----------------------------------------------
+def new_function():
+    """Function description here..."""  
+
+    return params_to_return_here
+
+#----------------------------------------------- End: Define main functions -----------------------------------------------
+
+
+#----------------------------------------------- Run script -----------------------------------------------
+if __name__ == "__main__":
+    # Add main script run commands here...
+
+    print("Finished")
